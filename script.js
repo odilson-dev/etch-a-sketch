43c74cb762cd7e0c0110a3460ebfbce5a7b2e0ff
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-02 16:23:24 -0400

    Randomize the square’s RGB value
    Rather than a simple color change from black to white, each interaction
    will randomize the square’s RGB value.

--- script.js
+++ script.js
@@ -7,30 +7,36 @@ container.style.display = "grid";
 
 let button = document.querySelector("button");
 
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
 
 function createSquares(squareNumber){
 
     let squareWidth = CONTAINER_WIDTH / squareNumber;
     container.style.gridTemplateColumns = `repeat(${squareNumber}, 1fr)`;
+    let color = `rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
     for(let i = 1; i <= (squareNumber**2); i++){
         const squareItem = document.createElement("div");
         squareItem.style.width = `${squareWidth}px`;
         squareItem.style.height = `${squareWidth}px`;
         squareItem.classList.add("square-item");
+        
+
         squareItem.addEventListener("mouseover", (e) => {
             if(e.buttons == 1 || e.buttons == 3){
-                squareItem.style.backgroundColor = "yellow";
+                squareItem.style.backgroundColor = color;
             }
-            
+        })
+        squareItem.addEventListener("mouseout", (e) => {
+            if(!(e.buttons == 1 || e.buttons == 3)){
+                color =`rgb(${getRandomInt(256)}, ${getRandomInt(256)}, ${getRandomInt(256)})`;
+            }
         })
         container.appendChild(squareItem);
-        
-    }
-
- 
+    } 
 }
-
-
 
 button.addEventListener("click", () =>{
     let userAnswer = parseInt(prompt("What is the number of squares per side for the new grid?"));
@@ -40,8 +46,6 @@ button.addEventListener("click", () =>{
     } else{
     container.innerHTML = '';
     createSquares(userAnswer);
-
-
     
 }
 });
