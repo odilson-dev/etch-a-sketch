5badef048ede571b616721d117e622fddacb8d48
Italis Odilson Woodly <italisodilsonwoodly@hotmail.com>
2023-09-01 22:39:30 -0400

    Add popup button to the top of the screen
    I added a button to the top of the screen that will send the user a
    popup asking for the number of squares per side for the new grid.

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 let container = document.querySelector(".container");
+container.style.display = "grid";
 
+let button = document.querySelector("button");
 
+button.addEventListener("click", () =>{
+    let useAnswer = parseInt(prompt("What is the number of squares per side for the new grid?"));
 
+    container.style.gridTemplateColumns = `repeat(${useAnswer}, 1fr)`;
+    for(let i = 1; i <= (useAnswer**2); i++){
+        const squareItem = document.createElement("div");
+        squareItem.classList.add("square-item");
+        squareItem.textContent = i;
+        container.appendChild(squareItem);
+    }
+});
+
+container.style.gridTemplateColumns = `repeat(16, 1fr)`;
 for(let i = 1; i <= 256; i++){
     const squareItem = document.createElement("div");
     squareItem.classList.add("square-item");
